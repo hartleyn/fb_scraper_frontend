49bbaf3f8aca4adbf1224ed68a7415635bca9fae
Nicholas Hartley <nhartley18@me.com>
2018-09-05 04:25:18 -0400

    Secure route added.

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export const ENV = 'PROD';
 export const LOCAL_BASE_PATH = 'http://127.0.0.1:8000/api/v1';
-export const PROD_BASE_PATH = 'http://hartleyn.pythonanywhere.com/api/v1';
+export const PROD_BASE_PATH = 'https://hartleyn.pythonanywhere.com/api/v1';
 export const BASE_PATH = (ENV === 'PROD') ? PROD_BASE_PATH : LOCAL_BASE_PATH;
